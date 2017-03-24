4fbbd99087150c834e5d683038433657623b5a7c
nikosioannidisconsulente <consulente.it12@percassi.it>
2017-03-24 10:17:29 +0100

    asd

--- PerParser/src/com/perparser/parsers/AmazonParser.java
+++ PerParser/src/com/perparser/parsers/AmazonParser.java
@@ -12,36 +12,37 @@ import org.springframework.stereotype.Service;
 
 import com.opencsv.CSVReader;
 
-
-
-
 @Service("AmazonParser")
 public class AmazonParser extends BaseParser {
 
-	private static final List<String> COLUMN_LIST = Arrays.asList("sku", "upc", "title", "brand", "manufacturer", "product_type", "item_type", "description", "part_number", "catalog_number");
+	private static final List<String> COLUMN_LIST = Arrays.asList("sku", "upc", "title", "brand", "manufacturer",
+			"product_type", "item_type", "description", "part_number", "catalog_number");
 	static final String TYPE = "Amazon";
 	private static final String TITLE = "Amazon File View";
-	
+
 	@Override
-	public JSONObject parseToJson(InputStream stream, String countryCode) throws IOException{
+	public JSONObject parseToJson(InputStream stream, String countryCode) throws IOException {
 		JSONArray list = new JSONArray();
 		JSONArray tmplist = new JSONArray();
 		JSONArray headersList = new JSONArray();
 		JSONObject ret = null;
 		InputStreamReader exp = new InputStreamReader(stream);
-		
-		CSVReader csvReader = new CSVReader(exp, '|');
+		int i = 0;
+		String thisLine;
+		BufferedReader myInput = new BufferedReader(exp);
+		while ((thisLine = myInput.readLine()) != null) {
+			String[] line = thisLine.split("\\|");
+			for (String item : line)
+				tmplist.add(item);
 
-	     String [] nextLine;
-	     while ((nextLine = csvReader.readNext()) != null) {
-	    	tmplist = new JSONArray(); 
-			tmplist.add(nextLine[0]);
-			tmplist.add(nextLine[1]);
-			tmplist.add(nextLine[2]);
 			list.add(tmplist);
-	     }
+			i++;
+			if(i==2)
+				break;
+		}
+
 		headersList = createHeaderList(COLUMN_LIST);
-		ret= createJSON(list, headersList, TYPE, TITLE);
+		ret = createJSON(list, headersList, TYPE, TITLE);
 		return ret;
 	}
 }
